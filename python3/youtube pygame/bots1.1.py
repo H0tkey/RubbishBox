f4e946a72299f329c3f18aa8cc035d3cf4c67c0b
H0tkey <romanmanechkin@gmail.com>
2018-08-02 22:27:35 +0700

    3rd commit

--- python3/youtube pygame/bots1.1.py
+++ python3/youtube pygame/bots1.1.py
@@ -6,7 +6,7 @@ WINDOW_HEIGHT = 600
 pygame.init()
 
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("Bot's world'!")
 
@@ -23,96 +23,92 @@ step = 30
 
 run = True
 
-Map = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-       [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+Map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 
 def drawWindow():
-    
-    startX = 5 
+
+    startX = 5
     startY = 5
-        
+
     global Map
-    
+
     window.fill((230, 230, 250))
-    
-    #for line in range(len(Map)):
-        #for sell in range(len(Map[line])):
-            #pygame.draw.rect(window,(100,100,100),(startX + (widht + border) * sell,startY + (height + border) * line,widht,height))
-            
-    
-    pygame.draw.rect(window,(0,0,255),(x,y,widht,height))
+
+    # for line in range(len(Map)):
+    # for sell in range(len(Map[line])):
+    #pygame.draw.rect(window,(100,100,100),(startX + (widht + border) * sell,startY + (height + border) * line,widht,height))
+
+    pygame.draw.rect(window, (0, 0, 255), (x, y, widht, height))
     pygame.display.update()
-    
+
 
 def handle_events():
-    
-    global xTarget,yTarget,run
-    
+
+    global xTarget, yTarget, run
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
+
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]: 
-        xTarget -= speed 
+    if keys[pygame.K_LEFT]:
+        xTarget -= speed
     if keys[pygame.K_RIGHT]:
-        xTarget += speed 
-    
+        xTarget += speed
+
     if keys[pygame.K_UP]:
-        yTarget -= speed 
+        yTarget -= speed
     if keys[pygame.K_DOWN]:
-        yTarget += speed 
-        
-    
+        yTarget += speed
+
 
 def update():
-    
+
     global x, y, xTarget, yTarget
-    
+
     if x != xTarget:
         if xTarget > x and xTarget <= WINDOW_WIDTH - widht:
             x += step
-            xTarget -=step
+            xTarget -= step
             print("ok")
         if xTarget < x and xTarget >= 5:
             x -= step
-            xTarget +=step
+            xTarget += step
     if y != yTarget:
         if yTarget > y and yTarget <= WINDOW_HEIGHT - height:
             y += step
             yTarget -= step
         if yTarget < y and yTarget >= 5:
             y -= step
-            yTarget +=step
-        
+            yTarget += step
+
     if x == WINDOW_WIDTH - widht or x == 5:
         xTarget = x
-    
+
     if y == WINDOW_HEIGHT - height or y == 5:
         yTarget = y
 
-    
-
 
 while run:
-    
+
     clock.tick(30)
     handle_events()
     update()
     drawWindow()
-    #print(x,xTarget)
-    #print(y,yTarget)
-    
-    
+    # print(x,xTarget)
+    # print(y,yTarget)
+
+
 pygame.quit()
